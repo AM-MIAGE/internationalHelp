0e74248b59eb0e69e3d4b81348a25393c9c589b2
AM-MIAGE <arles.mathieu@gmail.com>
2016-12-02 03:27:44 +0100

    suite page_succès

--- vues/success.php
+++ vues/success.php
@@ -2,7 +2,7 @@
 
 <html>
 <head>
-	<title>Connection</title>
+	<title>Scoring</title>
 	    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -28,13 +28,88 @@
 <div class="container-fluid">
 	
 <div class="row">
-<div class="form-group col-md-6 col-md-offset-3">
-	<h1>Score</h1>
+<div class="form-group col-md-8 col-md-offset-2">
+	<h1 class="text-center">Score</h1>
+	<p class="text-center">32</p>
+
+	<h1 class="text-center">Badges</h1>
+	<table class="table">
+		<thead>
+			<tr class="text-center">
+				<th>Gold Answer <img src="./images/coupe_or.png"/></th>
+				<th>Silver Answer <img src="./images/coupe_argent.png"/></th>
+				<th>Bronze Answer <img src="./images/coupe_bronze.png"/></th>
+				<th>Gold Question <img src="./images/étoile_or.png"/></th>
+				<th>Silver Question <img src="./images/étoile_argent.png"/></th>
+				<th>Bronze Question <img src="./images/étoile_bronze.png"/></th>
+				<th>Gold Translation <img src="./images/médaille_or.png"/></th>
+				<th>Silver Translation <img src="./images/médaille_argent.png"/></th>
+				<th>Bronze Translation <img src="./images/médaille_bronze.png"/></th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr class="text-center">
+				<td>3</td>
+				<td>1</td>
+				<td>5</td>
+				<td>0</td>
+				<td>6</td>
+				<td>7</td>
+				<td>3</td>
+				<td>6</td>
+				<td>1</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+</div>
 
 
-	<h2>Badges</h2>
+<div class="row">
+<div class="form-group col-md-8 col-md-offset-2">
 
 
+	<h1 class="text-center">Rank</h1>
+	<table class="table">
+		<thead>
+			<tr class="text-center">
+				<th>#</th>
+				<th>User</th>
+				<th>Score</th>
+
+			</tr>
+		</thead>
+		<tbody>
+			<tr class="text-center">
+				<td>1</td>
+				<td>Toto</td>
+				<td>200</td>
+			</tr>
+			<tr class="text-center">
+								<td>2</td>
+				<td>Titi</td>
+				<td>180</td>
+			</tr>
+			<tr class="text-center">
+								<td>3</td>
+
+				<td>Tata</td>
+				<td>150</td>
+			</tr>
+			<tr class="text-center">
+								<td>4</td>
+
+				<td>Tutu</td>
+				<td>90</td>
+			</tr>
+						<tr class="text-center">
+								<td>5</td>
+
+				<td>Bob</td>
+				<td>50</td>
+			</tr>
+		</tbody>
+	</table>
 </div>
 </div>
 
